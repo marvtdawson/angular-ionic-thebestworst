ee541fd1409c45f293b346b0795f99a0d0750a6a
Marvin Dawson <marvintdawson@gmail.com>
2018-08-26 21:28:05 -0500

    first initial commit

---
+++ src/components/auth-components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth/auth';
+@NgModule({
+	declarations: [AuthComponent],
+	imports: [],
+	exports: [AuthComponent]
+})
+export class AuthComponentsModule {}
